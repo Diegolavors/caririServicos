23a3d0a25e7bd17aad6af22e19d9fcded4440641
michellima193 <michellima193@gmail.com>
2017-06-07 13:27:37 -0300

    janela modal do login/começando cadTrabalhador

---
+++ application/views/pagCadTrabalhador.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Seus Dados</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('bootstrap-3.3.7-dist/css/bootstrap-theme.min'); ?>">
+        <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url('bootstrap-3.3.7-dist/css/bootstrap.css'); ?>">
+        <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url('bootstrap-3.3.7-dist/css/estilo2.css'); ?>">
+
+    </head>
+    <body>
+
+        <div class="container" style="padding-top: 40px;">
+            <div class="row">
+                <div class="col-xs-12">
+
+                    <div class="page-header">
+
+
+                        <h2>Anucie seus serviços grátis<small class="text-right titulo">Já fez seu Cadastro?<a href="">Entra</a></small></h2>  
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- chamando js-->
+
+    <script src="<?php echo base_url("bootstrap-3.3.7-dist/js/bootstrap.min.js"); ?>"></script>
+</body>
+</html>
